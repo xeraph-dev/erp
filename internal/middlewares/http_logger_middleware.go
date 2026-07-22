4d9d71080c73532ebd8158e05b8349ec78a4d0b7
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2026-07-22 15:06:38 -0400

    [api] log status code when existing request

--- internal/middlewares/http_logger_middleware.go
+++ internal/middlewares/http_logger_middleware.go
@@ -1,21 +1,39 @@
 package middlewares
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
 )
 
+type responseWriterWrapper struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func newResponseWriterWrapper(w http.ResponseWriter) *responseWriterWrapper {
+	return &responseWriterWrapper{ResponseWriter: w}
+}
+
+func (w *responseWriterWrapper) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
 func HTTPLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		wrapper := newResponseWriterWrapper(w)
+
 		ctx := r.Context()
 		logger := GetLogger(ctx)
 		logger.InfoContext(ctx, "entering request",
 			slog.String("addr", r.RemoteAddr),
 			slog.String("method", r.Method),
 			slog.String("uri", r.RequestURI))
-		// TODO: log response's status code
-		defer logger.InfoContext(ctx, "exiting request")
+		defer func() {
+			logger.InfoContext(ctx, "exiting request", "status", fmt.Sprintf("%d %s", wrapper.statusCode, http.StatusText(wrapper.statusCode)))
+		}()
 
-		next.ServeHTTP(w, r)
+		next.ServeHTTP(wrapper, r)
 	})
 }
