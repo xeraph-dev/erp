2f7c021b3cc16c6e9369abc901f251453aade62f
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2026-07-23 12:32:19 -0400

    [api] fix migration

--- db/migrations/000005_refresh_tokens.up.sql
+++ db/migrations/000005_refresh_tokens.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS erp.refresh_tokens (
     token_hash TEXT PRIMARY KEY,
     user_id UUID NOT NULL
-    REFERENCES erp.users (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    REFERENCES erp.users_table (id) ON UPDATE CASCADE ON DELETE CASCADE,
     family_id UUID NOT NULL,
     expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    revoked_at TIMESTAMP WITH TIME ZONE,
+    revoked_at TIMESTAMP WITH TIME ZONE
 );
